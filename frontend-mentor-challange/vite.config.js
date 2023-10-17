910af00a8ac5d382681120cdc066f33e93bc59fc
GreenT2012 <t.klattenhoff@gmail.com>
2023-10-17 23:58:51 +0200

    v1

--- frontend-mentor-challange/vite.config.js
+++ frontend-mentor-challange/vite.config.js
@@ -5,8 +5,4 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 })
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-      ? '/my-project/'
-      : '/'
-}
+
