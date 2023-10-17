87106a8ade94a766acfe9ba00764ebdd657b8b9a
GreenT2012 <t.klattenhoff@gmail.com>
2023-10-17 23:16:43 +0200

    v1

---
+++ README.md
@@ -0,0 +1,68 @@
+# Frontend Mentor - Interactive rating component solution
+
+This is a solution to the [Interactive rating component challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating
+
+### Screenshot
+
+![](screenshot_1.png)
+![](screenshot_2.png)
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [Vue](https://vuejs.org/) - JS library
+- [Tailwindcss](https://tailwindcss.com) - For styles
+
+### What I learned
+
+I learned how to create multiple elements using a loop (v-for). 
+Additionally, I learned to style with TailwindCSS. 
+This was my first project with Vue and TailwindCSS.
+
+### Useful resources
+
+- [Chat GPT](https://www.chat.openai.com) - There, I was able to ask more specific questions regarding my issues
+- [Tailwind Cheatsheet](https://tailwindcomponents.com/cheatsheet/) - Outstanding cheatsheet
+
+## Author
+
+- Website - [Add your name here](https://www.your-site.com)
+- Frontend Mentor - [@GreenT2012](https://www.frontendmentor.io/profile/greent2012)
---
+++ frontend-mentor-challange/src/components/HelloWorld.vue
@@ -0,0 +1,111 @@
+<script setup>
+import { ref } from 'vue'
+
+
+defineProps({
+  msg: String,
+
+
+})
+
+
+
+const count = ref(0)
+const previousActiveNumber =ref(0);
+const state = ref(0)
+
+
+function setCountAndHighlight(i) {
+  this.count = i;
+  this.previousActiveNumber = this.count; // Speichere die aktuelle aktive Nummer
+  this.activeNumber = i;
+  console.log(this.count);
+}
+
+</script>
+
+<template>
+  <div v-if="state === 0" class="original-bg container flex mx-auto p-5 bg-black sm:w-96 w-85 h-96  rounded-3xl flex-col justify-around ">
+    <img class="rounded-full medium-gray w-10 p-3" src="../assets/icon-star.svg" alt="">
+    <div class=" container mx-auto ">
+      <h3 class="text-left text-3xl font-bold mb-3">How did we do?</h3>
+      <p class="text-color font-bold text-sm text-left pl-px">Please let us know how we did with your support request.
+        All feedback is appreciated to help us improve our offering!</p>
+    </div>
+    <div class="text-color container numbers flex flex-row justify-center  gap-6">
+      <div
+        v-for="i in 5"
+        :key="i"
+        @click="setCountAndHighlight(i);"
+        :class="['circle', 'flex', 'container', 'medium-gray', 'font-bold', 'rounded-full', 'w-12', 'h-12', 'justify-center', 'items-center', (i === count ? 'active-number' : ''), ]">
+      {{i}}
+      </div>
+    </div>
+    <button @click="state=1"
+            class="bg-orange-500 hover:text-orange-500 border-0 hover:bg-white rounded-full uppercase tracking-widest-xl text-sm h-12 md:h-auto font-normal ">Submit</button>
+  </div>
+    <div v-else class="original-bg container flex mx-auto p-5 bg-black w-96 h-96  rounded-3xl flex-col justify-around pt-8 pb-8">
+    <img class="w-40 mx-auto  " src="../assets/illustration-thank-you.svg" alt="">
+  <div class="banner medium-gray rounded-3xl text-orange-400 w-48  mx-auto py-2">You selected {{count}} out of 5</div>
+    <h3 class="  text-3xl font-bold ">Thank You!</h3>
+    <p class="px-5 text-color font-bold text-sm  pl-px">We appreciate you taking the time to give a rating. If you ever need more support,
+      don’t hesitate to get in touch!</p>
+  </div>
+
+
+  <!-- Rating state end -->
+
+  <!-- Thank you state start -->
+
+
+
+  <!-- Thank you state end -->
+</template>
+
+
+<style scoped>
+@import url(https://fonts.google.com/specimen/Overpass);
+/* we will explain what these classes do next! */
+
+
+
+.circle:hover {
+  background-color:hsl(217, 12%, 63%);
+  cursor:pointer;
+  color:white;
+}
+circle{
+  color:hsl(216, 12%, 54%);
+}
+
+html,body {
+  font-family: 'Overpass', sans-serif!important;
+
+}
+.tracking-widest-xl{
+  letter-spacing: 0.2rem;
+}
+.medium-gray{
+background-color:hsl(213, 19%, 18%);
+}
+.orange{
+background-color:hsl(25, 97%, 53%);
+}
+
+.active-number{
+  background-color:hsl(25, 97%, 53%);
+  color:white;
+}
+.previous-active-color{
+  color:white;
+  background-color:hsl(217, 12%, 63%);
+}
+
+.original-bg{
+  background: radial-gradient(circle at top center, hsl(213, 19%, 18%) 5%, hsl(216, 12%, 8%)115%);
+}
+.text-color{
+color:hsl(216, 12%, 54%)
+}
+
+</style>
